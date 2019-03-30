cdc89ee04334cd91304ccbbbf9d3d7d6a5aec68c
HonzaBenes <janbenes1234@gmail.com>
2019-03-30 15:06:13 +0100

    Work in progress on threshold optimization

---
+++ find_threshold.py
@@ -0,0 +1,31 @@
+# 1) Compute distance matrix
+# 2) Iterate through distances which represent potential threshold
+# 3) Evaluate false acceptance rate and false rejection rate and plot them
+import h5py
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+from config import Config
+
+if __name__ == "__main__":
+    conf = Config()
+
+    # 1) Open the h5 file
+    with h5py.File(conf.DB_PATH, 'r') as h5f:
+        data = h5f['udalosti_-_11.11.2016_19-00_0.mp4'][0:1000]
+        names = h5f['udalosti_-_11.11.2016_19-00_0.mp4.names'][0:1000]
+        similarities = cosine_similarity(data)
+        indices = np.triu_indices(similarities.shape[0])
+        threshold_set = set(similarities[indices])
+        values = np.zeros((len(threshold_set), 3), dtype=np.float32)
+
+        dst_count = len(indices[0])
+        for i, threshold in enumerate(threshold_set):
+            values[i, 0] = threshold
+            fac = 0
+            frc = 0
+
+            for j in range(len(indices[0])):
+                coords = (indices[0][j], indices[1][j])
+                reference_affinity = (names[coords[0]] == names[coords[1]])
+                inferred_affinity = (similarities[coords] <= threshold)
