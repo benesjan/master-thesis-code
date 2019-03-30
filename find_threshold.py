2d44f52a5e8652ca54e739a2ab5f0fc9e2e48c81
HonzaBenes <janbenes1234@gmail.com>
2019-03-30 19:13:03 +0100

    Dataset merging

--- find_threshold.py
+++ find_threshold.py
@@ -1,6 +1,8 @@
 # 1) Compute distance matrix
 # 2) Iterate through distances which represent potential threshold
 # 3) Evaluate false acceptance rate and false rejection rate and plot them
+from time import time
+
 import h5py
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
@@ -10,17 +12,22 @@ from config import Config
 if __name__ == "__main__":
     conf = Config()
 
+    start_time = time()
+
     # 1) Open the h5 file
-    with h5py.File(conf.DB_PATH_RAW, 'r') as h5f:
-        data = h5f['udalosti_-_11.11.2016_19-00_0.mp4'][0:1000]
-        names = h5f['udalosti_-_11.11.2016_19-00_0.mp4.names'][0:1000]
+    with h5py.File(conf.DB_PATH, 'r') as h5f:
+        data = h5f['features']
+        names = h5f['names']
         similarities = cosine_similarity(data)
         indices = np.triu_indices(similarities.shape[0])
-        threshold_set = set(similarities[indices])
-        values = np.zeros((len(threshold_set), 3), dtype=np.float32)
+        # threshold_set = set(similarities[indices])
+
+        thresholds = np.arange(-1, 1, 0.01)
+
+        values = np.zeros((len(thresholds), 3), dtype=np.float32)
 
         dst_count = len(indices[0])
-        for i, threshold in enumerate(threshold_set):
+        for i, threshold in enumerate(np.arange(-1, 1, 0.001)):
             values[i, 0] = threshold
             fac = 0
             frc = 0
@@ -37,3 +44,5 @@ if __name__ == "__main__":
 
             values[i, 1] = fac / dst_count
             values[i, 2] = frc / dst_count
+
+    print(f"Processing finished in {(time() - start_time) / 60} minutes")
--- merge_datasets.py
+++ merge_datasets.py
@@ -1,4 +1,5 @@
 import h5py
+import numpy as np
 
 from config import Config
 
@@ -7,4 +8,9 @@ if __name__ == "__main__":
 
     # 3) Open the h5 file
     with h5py.File(conf.DB_PATH_RAW, 'r') as h5f_raw, h5py.File(conf.DB_PATH, 'w') as h5f:
-        pass
+        for dataset_name in h5f_raw.keys():
+            features = np.concatenate([h5f_raw[x] for x in h5f_raw.keys() if x.endswith("mp4")])
+            names = np.concatenate([h5f_raw[x] for x in h5f_raw.keys() if x.endswith("names")])
+
+            h5f.create_dataset("features", data=features)
+            h5f.create_dataset("names", data=names, dtype=h5py.special_dtype(vlen=str))
