846025ceb3daffaf18cc47056bf6dbaa757f6b8a
HonzaBenes <janbenes1234@gmail.com>
2019-04-04 14:38:18 +0200

    Work in progress on parallelization

--- find_threshold.py
+++ find_threshold.py
@@ -1,49 +1,35 @@
 # 1) Compute distance matrix
 # 2) Iterate through distances which represent potential threshold
 # 3) Evaluate false acceptance rate and false rejection rate and plot them
-from time import time
 
 import h5py
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
 
 from config import Config
+
+
+def generate_intervals(max_val, interval_len):
+    vals = list(range(0, max_val, interval_len))
+    vals.append(max_val)
+    return [(vals[i], vals[i + 1]) for i in range(len(vals) - 1)]
+
+
+def get_next_pair(intervals):
+    for i in range(len(intervals)):
+        for j in range(i, len(intervals)):
+            yield [intervals[i], intervals[j]]
+
 
 if __name__ == "__main__":
     conf = Config()
 
-    start_time = time()
-
     # 1) Open the h5 file
     with h5py.File(conf.DB_PATH, 'r') as h5f, h5py.File(conf.THRESHOLD_VALS, 'w') as h5t:
-        data = h5f['features']
-        names = h5f['names']
-        similarities = cosine_similarity(data)
-        indices = np.triu_indices(similarities.shape[0])
-        # threshold_set = set(similarities[indices])
+        FEATURES = h5f['features']
+        NAMES = h5f['names']
 
-        thresholds = np.arange(-1, 1, 0.01)
+        THRESHOLDS = np.arange(0, 1, 0.005)
 
-        vals = np.zeros((len(thresholds), 3), dtype=np.float32)
-
-        dst_count = len(indices[0])
-        for i, threshold in enumerate(thresholds):
-            vals[i, 0] = threshold
-            fac = 0.
-            frc = 0.
-
-            for j in range(len(indices[0])):
-                coords = (indices[0][j], indices[1][j])
-                reference_affinity = (names[coords[0]] == names[coords[1]])
-                inferred_affinity = (similarities[coords] <= threshold)
-                if inferred_affinity and not reference_affinity:
-                    fac += 1
-                elif reference_affinity and not inferred_affinity:
-                    frc += 1
-
-            vals[i, 1] = fac / dst_count
-            vals[i, 2] = frc / dst_count
-
-        h5t.create_dataset("vals", data=vals)
-
-    print(f"Processing finished in {(time() - start_time) / 60} minutes")
+        intervals = generate_intervals(FEATURES.shape[0], 100000)
+        for pair in get_next_pair(intervals):
+            print(pair)
