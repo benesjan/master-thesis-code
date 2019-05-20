Overflow bug fix

find_threshold.py
@@ -28,7 +28,7 @@ def get_next_pair(intervals):
 def process_distances(interval_pair):
     dists = cosine_distances(FEATURES[interval_pair[0][0]:interval_pair[0][1]],
                              FEATURES[interval_pair[1][0]:interval_pair[1][1]])
-    vals = np.zeros((len(THRESHOLDS), 4), dtype=np.uint32)
+    vals = np.zeros((len(THRESHOLDS), 4), dtype=np.uint64)
 
     labels1 = LABELS[interval_pair[0][0]:interval_pair[0][1]]
     labels2 = LABELS[interval_pair[1][0]:interval_pair[1][1]]
@@ -72,13 +72,13 @@ if __name__ == "__main__":
         LABELS = h5f['labels']
 
         THRESHOLDS = np.arange(0, 2, 0.005)
-        INTERVALS = generate_intervals(FEATURES.shape[0], 4000)
+        INTERVALS = generate_intervals(FEATURES.shape[0], 10000)
 
         NUM_PAIRS = len(INTERVALS) * (len(INTERVALS) + 1) / 2
 
         pool = mp.Pool(conf.CPU_COUNT)
 
-        result = np.zeros((len(THRESHOLDS), 4), dtype=np.uint32)
+        result = np.zeros((len(THRESHOLDS), 4), dtype=np.uint64)
         for processed_count, res_x in enumerate(pool.imap(process_distances, get_next_pair(INTERVALS)), 1):
             result += res_x
 