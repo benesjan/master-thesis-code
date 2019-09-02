3f971bcdc6abe6b65bd72b50afb5edc7652ec713
HonzaBenes <janbenes1234@gmail.com>
2019-09-02 18:11:19 +0200

    Accent stripping bug fix

--- utils.py
+++ utils.py
@@ -9,5 +9,5 @@ def create_dir(path):
 
 def strip_accents(s):
     """Replaces accentual characters with ASCII counterparts, or removes them if counterpart not present"""
-    return ''.join(c for c in unicodedata.normalize('NFD', s)
-                   if unicodedata.category(c) != 'Mn').encode('ascii', 'ignore')
+    return str(''.join(c for c in unicodedata.normalize('NFD', s)
+                   if unicodedata.category(c) != 'Mn').encode('ascii', 'ignore'), 'utf-8')
