4bd20827b2aa82a6d5b7d69c409b6c129d72542f
HonzaBenes <janbenes1234@gmail.com>
2019-09-04 22:31:21 +0200

    Update to GPU script

--- run_gpu.sh
+++ run_gpu.sh
@@ -4,18 +4,15 @@
 #PBS -q gpu
 
 module add python-3.6.2-gcc
-module add python36-modules-gcc
-module add pytorch-1.1.0_python-3.6.2_cuda-10.1
-module add opencv-3.4.5-py36-cuda10.1
 
 home_dir="/storage/plzen1/home/benesjan"
 timestamp=$(date +%s)
 
 export TMPDIR=$home_dir/tmp
 
-virtualenv $home_dir/myenv
-source $home_dir/myenv/bin/activate
+source $home_dir/spc/venv/bin/activate
 
-pip install git+https://github.com/benesjan/mtcnn-pytorch.git
+module add pytorch-1.1.0_python-3.6.2_cuda-10.1
+module add opencv-3.4.5-py36-cuda10.1
 
-python "$home_dir/spc/$SCRIPT_NAME.py" > "$home_dir/output_gpu_$timestamp.txt"
+python "$home_dir/spc/$SCRIPT_NAME.py"
